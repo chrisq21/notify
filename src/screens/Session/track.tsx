18d18c50cddad8d6a925cdf3dfbcc9098a5ba434
Chris Queen <chrisqueen10@gmail.com>
2021-03-27 21:54:11 -0400

    Added track marker

--- src/screens/Session/track.tsx
+++ src/screens/Session/track.tsx
@@ -2,21 +2,20 @@ import React, {useEffect, useRef, useState} from 'react';
 import {View, Text, Animated, Easing, Modal} from 'react-native';
 import styled from '@emotion/native';
 import {formatTime} from '../../lib/helpers/formatTime';
+
 const Container = styled.View`
   flex: 1;
 `;
 
 const NotificationContainer = styled.TouchableHighlight`
   position: absolute;
-  width: 100%;
+  width: 90%;
   padding: 10px 18px;
   background: rgba(67, 59, 70, 0.8);
-
   shadow-color: black;
   shadow-offset: 0px 0px;
   shadow-opacity: 0.6;
   shadow-radius: 5px;
-
   border-radius: 8px;
 `;
 
@@ -32,8 +31,48 @@ const NotificationTime = styled.Text`
 
 const TrackLine = styled(Animated.View)`
   height: 2px;
-  background: #4a4157;
+  background-color: #4a4157;
   z-index: 2;
+`;
+
+const TrackMarkerSmall = styled.View`
+  position: absolute;
+  top: -5px;
+  right: -5px;
+  width: 10px;
+  height: 10px;
+  background-color: white;
+  border-radius: 1000px;
+`;
+
+const TrackMarkerMedium = styled.View`
+  position: absolute;
+  top: -13px;
+  right: -13px;
+  width: 26px;
+  height: 26px;
+  background-color: rgb(150, 60, 187);
+  border-radius: 1000px;
+`;
+
+const TrackMarkerLarge = styled.View`
+  position: absolute;
+  top: -17px;
+  right: -17px;
+  width: 34px;
+  height: 34px;
+  background-color: rgba(123, 49, 153, 0.4);
+  border-radius: 1000px;
+`;
+
+const VerticalTrackContainer = styled.View`
+  position: absolute;
+  right: -3px;
+  top: 0;
+  height: 100%;
+  width: 6px;
+  background-color: #4a4157;
+  border-radius: 10px;
 `;
 
 const ModalCenteredView = styled.View`
@@ -68,11 +107,15 @@ const Track = ({elapsedTime, endTime, notifications, isSessionActive}) => {
       onLayout={(event) => {
         setContainerHeight(event.nativeEvent.layout.height);
       }}>
+      <VerticalTrackContainer />
       <TrackLine
         style={{
           transform: [{translateY: trackPosition}],
-        }}
-      />
+        }}>
+        <TrackMarkerMedium />
+        <TrackMarkerLarge />
+        <TrackMarkerSmall />
+      </TrackLine>
       <Modal
         animationType="slide"
         transparent={true}
